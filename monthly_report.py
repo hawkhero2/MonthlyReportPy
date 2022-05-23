63a5a859fbde4b4ed56cca0d2d027cb57284091a
Ivanciu Andrei <cheseecheseechesee@gmail.com>
2022-05-23 22:24:11 +0300

    added labels + textboxes for dates & name& user acc

--- monthly_report.py
+++ monthly_report.py
@@ -1,7 +1,8 @@
+from distutils.command.build_scripts import first_line_re
 import sys
 import json
 from unicodedata import name
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableView, QTableWidget, QWidget, QPushButton, QFileDialog, QTableWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QTableView, QTableWidget, QWidget, QPushButton, QFileDialog, QTableWidget
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import QIcon
 from openpyxl import Workbook
@@ -26,26 +27,66 @@ class App(QMainWindow):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.statusBar().showMessage('Select master excel')
-        self.setFixedSize(300,150)
+        self.setFixedSize(450,250)
         
-        #Browse button
+        # Browse button
         button = QPushButton('browse',self)
         button.setToolTip('select excel file')
-        button.move(20,20)
+        button.move(20,180)
         button.clicked.connect(self.openFileNameDialog)
+        
+        # Qlabel name 
+        person_name = QLabel(self)
+        person_name.move(20,4)
+        person_name.setText('Full Name')
+        
+        #Qlabel account name
+        user_account = QLabel(self)
+        user_account.move(140,4)
+        user_account.setText('Account')
+        
+        # Create textbox name
+        self.textbox = QLineEdit(self)
+        self.textbox.move(20, 30)
+        self.textbox.resize(100,20)
+        
+        # Create textbox user_account
+        self.textbox = QLineEdit(self)
+        self.textbox.move(140, 30)
+        self.textbox.resize(100,20)
+        
+        # Create textbox first date
+        self.textbox = QLineEdit(self)
+        self.textbox.move(20, 100)
+        self.textbox.resize(100,20)
+        
+        
+        # Create textbox last date
+        self.textbox = QLineEdit(self)
+        self.textbox.move(140, 100)
+        self.textbox.resize(100,20)
+        
+        #QLabel first date
+        first_date = QLabel(self)
+        first_date.move(20, 75)
+        first_date.setText('First Date')
+        
+        #QLabel last date
+        first_date = QLabel(self)
+        first_date.move(140, 75)
+        first_date.setText('Last Date')
         
         self.show()
     
     #Open File Dialog Window
     def openFileNameDialog(self):
-        #Open File
-        # options = QFileDialog.Options()
-        # options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self,"Select Excel", "","Excel Files(*.xlsx)")
         
-        #load_workbook from fileName
+        fileName, _ = QFileDialog.getOpenFileName(self,"Select Excel", "","Excel Files(*.xlsx)") #Grab File
+        
         workbook = Workbook()
-        workbook = load_workbook(fileName)
+        destination_workbook = Workbook() #inst destination_workbook
+        # destination_workbook.save('c:/users/currentuser/desktop/'+operator_name+'.xlsx') #create destination_workbook
+        workbook = load_workbook(fileName) #load_workbook from fileName
         worksheet = workbook.active
         for iteration in worksheet.iter_rows(min_row=1, max_col=2, values_only=True): #returns a tuple
             if (iteration[0]==4):
